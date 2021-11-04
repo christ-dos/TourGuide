749e4231037754b640239c78a03b29fc99d93f20
Christine Duarte <christine_dos_santos@hotmail.fr>
2021-11-04 11:45:31 +0100

    implementation test for method trackUserLocation and getUserByUserName

--- microservice-gps/src/test/java/com/tripMaster/microservicegps/service/UserGpsServiceImplTest.java
+++ microservice-gps/src/test/java/com/tripMaster/microservicegps/service/UserGpsServiceImplTest.java
@@ -8,19 +8,15 @@ import gpsUtil.location.VisitedLocation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.util.Date;
-import java.util.Locale;
-import java.util.UUID;
+import static org.mockito.Mockito.*;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -28,27 +24,102 @@ public class UserGpsServiceImplTest {
 
     private UserGpsServiceImpl userGpsServiceTest;
 
-    private GpsUtil gpsUtil = new GpsUtil();
+    @Mock
+    private GpsUtil gpsUtilMock;
 
-    @Autowired
-    private InternalUserMapDAO internalUserMapDAO;
+    @Mock
+    private InternalUserMapDAO internalUserMapDAOMock;
+
+    private User userTest;
 
 
     @BeforeEach
     public void setUpPerTest() {
-        userGpsServiceTest = new UserGpsServiceImpl(gpsUtil,internalUserMapDAO);
-    }
+        userGpsServiceTest = new UserGpsServiceImpl(gpsUtilMock, internalUserMapDAOMock);
+        userTest = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
 
+    }
 
     @Test
-    public void getUserLocationTest() {
-        //GIVEN
-        User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
+    public void trackUserLocationTest(){
+        VisitedLocation visitedLocationMock = new VisitedLocation(userTest.getUserId(), new Location(33.817595D, -116.922008D), new Date());
+
+        when(gpsUtilMock.getUserLocation(userTest.getUserId())).thenReturn(visitedLocationMock);
         //WHEN
-        VisitedLocation visitedLocationResult = userGpsServiceTest.trackUserLocation(user);
+        assertTrue(userTest.getVisitedLocations().isEmpty());
+        VisitedLocation visitedLocationResult = userGpsServiceTest.trackUserLocation(userTest);
         //THEN
-        assertTrue(visitedLocationResult.userId.equals(user.getUserId()));
-        assertNotNull(visitedLocationResult.location);
+        assertTrue(!userTest.getVisitedLocations().isEmpty());
+        assertEquals(-116.922008D, visitedLocationResult.location.longitude);
+        assertEquals(33.817595D, visitedLocationResult.location.latitude);
+
+        verify(gpsUtilMock, times(1)).getUserLocation(any(UUID.class));
     }
+
+    @Test
+    public void getUserLocationTest_whenVisitedLocationsListIsEmpty_thenCallMethodTrackUserLoaction() {
+        //GIVEN
+        VisitedLocation visitedLocationMock = new VisitedLocation(userTest.getUserId(), new Location(33.817595D, -116.922008D), new Date());
+        List<VisitedLocation> emptyListTest = new ArrayList<>();
+        userTest.setVisitedLocations(emptyListTest);
+
+        when(gpsUtilMock.getUserLocation(userTest.getUserId())).thenReturn(visitedLocationMock);
+        //WHEN
+        assertTrue(userTest.getVisitedLocations().isEmpty());
+        VisitedLocation visitedLocationResult = userGpsServiceTest.getUserLocation(userTest);
+        //THEN
+        assertTrue(visitedLocationResult.userId.equals(userTest.getUserId()));
+        assertTrue(userTest.getVisitedLocations().size() > 0);
+        assertEquals(-116.922008D,visitedLocationResult.location.longitude);
+        assertEquals(33.817595D,visitedLocationResult.location.latitude);
+
+        verify(gpsUtilMock, times(1)).getUserLocation(any(UUID.class));
+    }
+
+    @Test
+    public void getUserLocationTest_whenVisitedLocationsListIsNotEmpty_thenGetLastVisitedLocation() {
+        //GIVEN
+        User userTest2 = new User(UUID.randomUUID(), "jona", "000", "jona@tourGuide.com");
+
+        List<VisitedLocation> visitedLocationListTest = Arrays.asList(
+                new VisitedLocation(userTest2.getUserId(), new Location(33.817595D, -116.922008D), new Date()),
+                new VisitedLocation(userTest2.getUserId(), new Location(34.817595D, -117.922008D), new Date()),
+                new VisitedLocation(userTest2.getUserId(), new Location(35.817595D, -118.922008D), new Date())
+        );
+        userTest2.setVisitedLocations(visitedLocationListTest);
+        //WHEN
+        VisitedLocation visitedLocationResult = userGpsServiceTest.getUserLocation(userTest2);
+        //THEN
+        assertEquals(userTest2.getUserId(),visitedLocationResult.userId);
+        assertEquals(visitedLocationListTest.get(2),visitedLocationResult);
+        assertTrue(userTest2.getVisitedLocations().size() > 0);
+        assertEquals(-118.922008D, visitedLocationResult.location.longitude);
+        assertEquals(35.817595D, visitedLocationResult.location.latitude);
+    }
+
+    @Test
+    public void getUserByUserNameTest_whenUserExistAndIsJon_thenReturnUserJon() {
+        //GIVEN
+        when(internalUserMapDAOMock.getUser(anyString())).thenReturn(userTest);
+        //WHEN
+        User userResult = userGpsServiceTest.getUserByUserName("jon");
+        //THEN
+        assertTrue(userResult.getUserId() == userTest.getUserId());
+        assertTrue(userResult.getEmailAddress().contains("jon"));
+        verify(internalUserMapDAOMock,times(1)).getUser(anyString());
+    }
+
+    @Test
+    public void getUserByUserNameTest_whenUserNotExistAndIsUnknown_thenReturnNull() {
+        //GIVEN
+        when(internalUserMapDAOMock.getUser(anyString())).thenReturn(null);
+        //WHEN
+        User userResult = userGpsServiceTest.getUserByUserName("unknown");
+        //THEN
+        assertNull(userResult);
+        verify(internalUserMapDAOMock,times(1)).getUser(anyString());
+    }
+
+
 
 }
