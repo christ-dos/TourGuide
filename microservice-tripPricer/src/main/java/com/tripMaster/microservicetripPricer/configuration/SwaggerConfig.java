d39275007bd65090fd7268c66ee5499a5cd64150
Christine Duarte <christine_dos_santos@hotmail.fr>
2021-11-22 15:43:24 +0100

    Writing javadoc forall classes of test

--- microservice-tripPricer/src/main/java/com/tripMaster/microservicetripPricer/configuration/SwaggerConfig.java
+++ microservice-tripPricer/src/main/java/com/tripMaster/microservicetripPricer/configuration/SwaggerConfig.java
@@ -12,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * Class which configures Swagger documentation
  *
  * @author Christine Duarte
+ *
  */
 @Configuration
 @EnableSwagger2
